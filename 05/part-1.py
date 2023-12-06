66b1573f9c3737724f146bb9818cd9590525df4e
Matt Maul <mmaul@criticalmention.com>
2023-12-05 23:18:01 -0500

    Day 5 maybe working but slow.

---
+++ 05/part-1.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+
+import copy
+import re
+import sys
+
+class Almanac:
+
+	def __init__(self):
+		self.maps = {}
+		self.map_order = []
+		self.seeds = []
+		self.input_min = None
+		self.input_max = None
+
+	def parse_input(self):
+
+		for line in sys.stdin:
+			if 'seeds' in line:
+				self.seeds = [int(seed) for seed in line.split(':')[1].split()]
+
+			elif 'map' in line:
+				map_name = line.split()[0]
+				self.maps[map_name] = []
+				self.map_order.append(map_name)
+			elif re.match('^[0-9 ]+$', line):
+				destination, source, length = line.split()
+				m = {
+						'destination': int(destination),
+						'source': int(source),
+						'length': int(length),
+						'source_max': int(source) + int(length)
+					}
+
+				if self.input_min is None or m['source'] < self.input_min:
+					self.input_min = m['source']
+				if self.input_max is None or m['source_max'] > self.input_max:
+					self.input_max = m['source_max']
+				self.maps[map_name].append(m)
+
+		for map_name, maps in self.maps.items():
+			self.maps[map_name] = sorted(maps, key=lambda m: m['source'])
+		print(self.maps)
+
+	def seed_location(self, seed_number):
+		output = seed_number
+		for map_name in self.map_order:
+			#print(f'{output} {map_name} ', end='')
+			if (output < self.maps[map_name][0]['source']
+					or output > self.maps[map_name][-1]['source_max']):
+				# Ranges are sorted. Skip if we're less than min or greater than max.
+				next
+			for curr_map in self.maps[map_name]:
+				if output >= curr_map['source'] and output < (curr_map['source'] + curr_map['length']):
+					output = curr_map['destination'] + (output - curr_map['source'])
+					break
+				elif output < curr_map['source']:
+					# It's too small, and they're sorted.
+					break
+		#print(output)
+		return(output)
+
+
+if __name__ == '__main__':
+	almanac = Almanac()
+	almanac.parse_input()
+	min_location = None
+	for seed in almanac.seeds:
+		location = almanac.seed_location(seed)
+		if min_location is None or location < min_location:
+			min_location = location
+	print(f'Part 1 minimum location is {min_location}.')
+
+	min_location = None
+	seeds = copy.deepcopy(almanac.seeds)
+	while(seeds):
+		print(seeds)
+		start_seed = seeds.pop(0)
+		length = seeds.pop(0)
+		for seed in range(start_seed, start_seed + length):
+			if seed < almanac.input_min or seed > almanac.input_max:
+				location = seed
+			else:
+				location = almanac.seed_location(seed)
+			if min_location is None or location < min_location:
+				min_location = location
+	print(f'Part 2 minimum location is {min_location}.')
